7d0757d46527c893a6d423ec81653faaa98dec6a
AbhyasKanaujia <44172451+AbhyasKanaujia@users.noreply.github.com>
2022-06-14 01:14:58 +0530

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import products from './data/products.js'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
-dotenv.config()
 
 app.get('/', (req, res) => {
   res.send('Api is running....')
