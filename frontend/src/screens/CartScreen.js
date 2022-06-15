9e1cc6e29b2ee5e913989dd2eff2cd63aa4c64dc
AbhyasKanaujia <44172451+AbhyasKanaujia@users.noreply.github.com>
2022-06-15 23:08:04 +0530

    connect cart Screen to redux

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useLocation, useParams } from 'react-router-dom'
+import { addToCart } from '../actions/cartActions'
 
 const CartScreen = () => {
-  return <h1>CartScreen</h1>
+  const location = useLocation()
+  const productId = useParams().id
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+  const dispatch = useDispatch()
+  const { cartItems } = useSelector((state) => state.cart)
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
+  return <h1>{JSON.stringify(cartItems)}</h1>
 }
 
 export default CartScreen
