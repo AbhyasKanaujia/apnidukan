573a4d449c096f769ee31be3006b2e34a1d6dc00
AbhyasKanaujia <44172451+AbhyasKanaujia@users.noreply.github.com>
2022-12-02 19:59:54 +0530

    Error handling in the backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import products from './data/products.js'
 import dotenv from 'dotenv'
 import colors from 'colors'
 
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 dotenv.config()
@@ -23,10 +24,11 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
+app.use(notFound)
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () =>
-  console.log(
-    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
