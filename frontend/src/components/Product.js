51a1d141431e52670ec0a5856af6ece1efd3199a
AbhyasKanaujia <44172451+AbhyasKanaujia@users.noreply.github.com>
2022-12-02 15:33:33 +0530

    HomeScreen Product Listing

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,22 @@
+import { Card } from 'react-bootstrap'
+
+const Product = ({ product }) => {
+  return (
+    <a href={`/product/${product._id}`} className="text-decoration-none">
+      <Card className="my-3 p-3 rounded">
+        <Card.Img src={product.image} variant="top" />
+
+        <Card.Body>
+          <Card.Title>
+            <strong>{product.name}</strong>
+          </Card.Title>
+          <Card.Text>${product.price}</Card.Text>
+          <Card.Text as="h6">Seller: {product.seller}</Card.Text>
+          <Card.Text as="h6">Address: {product.address}</Card.Text>
+        </Card.Body>
+      </Card>
+    </a>
+  )
+}
+
+export default Product
