aea2f86d0acaf3f806fb68c1bb39f9ecbd66a4ef
AbhyasKanaujia <44172451+AbhyasKanaujia@users.noreply.github.com>
2022-12-03 02:53:58 +0530

    add admin auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,7 @@ import {
   getUserProfile,
   updateUserProfile,
 } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
